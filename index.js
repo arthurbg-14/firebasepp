c9d20e3e39cf66fbf69eeef3a926df08a586bb4e
Arthur <arthurbragagrimaldi14@gmail.com>
2024-01-25 11:44:28 -0300

    typescript compiled

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sumQueryField = exports.countQuery = void 0;
+var firestore_1 = require("firebase/firestore");
+var countQuery = function (query) {
+    return (0, firestore_1.getCountFromServer)(query).then(function (doc) { return doc.data().count; });
+};
+exports.countQuery = countQuery;
+var sumQueryField = function (field) {
+    return function (query) {
+        return (0, firestore_1.getAggregateFromServer)(query, { sum: (0, firestore_1.sum)(field) }).then(function (doc) { return doc.data().sum; });
+    };
+};
+exports.sumQueryField = sumQueryField;
