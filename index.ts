79da63e80aa32f207739956b280079a2fdb3faa7
Arthur <arthurbragagrimaldi14@gmail.com>
2024-01-25 11:10:32 -0300

    add countQuery and sumQueryField

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { Query, DocumentData, getCountFromServer, getAggregateFromServer, sum } from 'firebase/firestore'
+
+export const countQuery = <T, J extends DocumentData>(query: Query<T, J>) => 
+    getCountFromServer(query).then(doc => doc.data().count)
+
+export const sumQueryField = <T, J extends DocumentData>(field: string) => 
+    (query: Query<T, J>) => 
+        getAggregateFromServer(query, {sum: sum(field)}).then(doc => doc.data().sum)
