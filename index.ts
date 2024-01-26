c4c1a78627e3b81d383d38d0d1f3081a8584c139
arthur <arthurbraga_99@hotmail.com>
2024-01-26 15:12:23 -0300

    add getRefField

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-import { Query, DocumentData, getCountFromServer, getAggregateFromServer, sum } from '@angular/fire/firestore'
+import { Query, DocumentData, getCountFromServer, getAggregateFromServer, sum, getDoc, doc, Firestore, FieldPath, DocumentReference } from '@angular/fire/firestore'
 
-export const countQuery = <T, J extends DocumentData>(query: Query<T, J>) => 
+export const countQuery = <M, DB extends DocumentData>(query: Query<M, DB>) => 
     getCountFromServer(query).then(doc => doc.data().count)
 
-export const sumQueryField = <T, J extends DocumentData>(field: string) => 
-    (query: Query<T, J>) => 
+export const sumQueryField = <M, DB extends DocumentData>(field: string) => 
+    (query: Query<M, DB>) => 
         getAggregateFromServer(query, {sum: sum(field)}).then(doc => doc.data().sum)
+
+export const getRefField = <M, DB extends DocumentData>(doc_ref: DocumentReference<M, DB>) => 
+    (field: string | FieldPath) => getDoc(doc_ref).then(doc => doc.get(field))
