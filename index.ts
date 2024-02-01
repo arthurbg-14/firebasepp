a4a2b27fb807521fd7835788f99de751816d6f58
arthur <arthurbraga_99@hotmail.com>
2024-02-01 15:58:09 -0300

    add observe data

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { Query, DocumentData, getCountFromServer, getAggregateFromServer, sum, getDoc, FieldPath, DocumentReference, getDocs, query, where } from '@angular/fire/firestore'
+import { Query, DocumentData, getCountFromServer, getAggregateFromServer, sum, getDoc, FieldPath, DocumentReference, getDocs, query, where, docSnapshots, collectionSnapshots } from '@angular/fire/firestore'
+import { map } from 'rxjs'
 
 export const countQuery = <M, DB extends DocumentData>(query_ref: Query<M, DB>) => 
     getCountFromServer(query_ref).then(doc => doc.data().count)
@@ -10,11 +11,20 @@ export const sumQueryField = <M, DB extends DocumentData>(field: string) =>
 export const getRefField = <M, DB extends DocumentData>(doc_ref: DocumentReference<M, DB>) => 
     (field: string | FieldPath) => getDoc(doc_ref).then(doc => doc.get(field))
 
+export const observeRefField = <M, DB extends DocumentData>(doc_ref: DocumentReference<M, DB>) => 
+    (field: string | FieldPath) => docSnapshots(doc_ref).pipe(map(doc => doc.get(field)))
+
 export const getData = <M, DB extends DocumentData>(doc_ref: DocumentReference<M, DB>) => 
     getDoc(doc_ref).then(doc => doc.data())
 
+export const observeData = <M, DB extends DocumentData>(doc_ref: DocumentReference<M, DB>) => 
+    docSnapshots(doc_ref).pipe(map(doc => doc.data()))
+
 export const getQueryData = <M, DB extends DocumentData>(query_ref: Query<M, DB>) => 
     getDocs(query_ref).then(query => query.docs.map(doc => doc.data()))
+
+export const observeQueryData = <M, DB extends DocumentData>(query_ref: Query<M, DB>) => 
+    collectionSnapshots(query_ref).pipe(map(docs => docs.map(doc => doc.data())))
 
 export const querySubstring = <M, DB extends DocumentData>(query_ref: Query<M, DB>) => 
     (field: string) => (text: string) => query(query_ref, where(field, '>=', text), where(field, '<=', text + '\uf8ff'))
